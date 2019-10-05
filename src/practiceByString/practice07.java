7b764c5be37d3a9dd9b245e398a88a697ead25e8
Aditya Singh Chauhan <aditya.chauhan_cs18@gla.ac.in>
2019-10-05 23:31:26 +0530

    Update practice07.java

--- src/practiceByString/practice07.java
+++ src/practiceByString/practice07.java
@@ -2,35 +2,19 @@ package practiceByString;
 import java.util.Scanner;
 
 public class practice07 {
-    
-    public static void main(String[] args){
 
-        Scanner sc= new Scanner(System.in);
-
-        String stringInput = sc.nextLine();
-
-        int lenOfStr = stringInput.length();
-
-        int totalWeight=0;
-
-        System.out.printf("totalWeight = ");
-
-
-
-        for (int i = 0; i < lenOfStr ; i++) {
-
-            int weightOfChar = (int) stringInput.charAt(i);
-
-            totalWeight = totalWeight + weightOfChar;
-
-            System.out.print( weightOfChar + " + ");
-
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the string =");
+            String Input = sc.nextLine();
+            int tweight = 0;
+            int length = Input.length();
+            System.out.printf("totalWeight = ");
+            for (int i = 0; i < length; i++) {
+                int weight = (int) Input.charAt(i);
+                tweight = tweight + weight;
+                System.out.print( weight + " + ");
+            }
+            System.out.print("\b\b \nTOTAL WEIGHT =" + tweight);
         }
-
-        System.out.println("\b\b \ntotalWeight = "+ totalWeight);
-
-
-
-    }
-
 }
