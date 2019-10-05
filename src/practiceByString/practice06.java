d98b3e8e7fafa66580db59fa9377a7c62ef35f97
dell <aditya.chauhan_cs18@gla.ac.in>
2019-10-05 22:18:53 +0530

    uppercase

---
+++ src/practiceByString/practice06.java
@@ -0,0 +1,25 @@
+package practiceByString;
+import java.util.Scanner;
+
+public class practice06 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Enter String : ");
+
+        String st1 = sc.nextLine();
+
+
+
+        st1 = st1.toUpperCase();
+
+        System.out.println("Upper Case : " + st1);
+
+
+
+    }
+
+}
+
